da2654e9f4eb049a31f103f29daf17e1d7a846b3
abeshi03 <k_913411@yahoo.co.jp>
2022-02-15 00:13:04 +0900

    ユーザー一覧ページ: 基本実装完了

--- src/pages/users/index.page.tsx
+++ src/pages/users/index.page.tsx
@@ -1,11 +1,109 @@
 /* --- フレームワーク、ライブラリー --------------------------------------------------------------------------------------- */
-import { VFC } from "react";
+import { useState, VFC } from "react";
+import useSWR from 'swr'
+
+/* --- アセット ------------------------------------------------------------------------------------------------------- */
+import styles from "./usersListPage.module.scss";
+
+/* --- フェッチャー ---------------------------------------------------------------------------------------------------- */
+import { getUsersFetcher } from "../../apis/UsersApi";
+
+/* --- 型定義 --------------------------------------------------------------------------------------------------------- */
+import { UsersResponseType } from "../../type/response/UsersResponse";
+
+/* --- エンドポイント -------------------------------------------------------------------------------------------------- */
+import { Endpoint } from "../../constants/endpoints";
+
+/* --- 子コンポーネント ------------------------------------------------------------------------------------------------ */
+import { Pagination } from "../../components/molecules/Pagination/Pagination";
+import { UserCard } from "../../components/organisms/Card/UserCard/UserCard";
+import { SearchInputField } from "../../components/atoms/SearchInputField/SearchInputField";
+
 
 const UsersListPage: VFC = () => {
 
+
+  const [ paginationPageNumber, setPaginationPageNumber ] = useState(1);
+  const [ searchWard, setSearchWard ] = useState<string>("");
+  const PER_PAGE_NUMBER: number = 15;
+
+
+  const { data, error } = useSWR<UsersResponseType>(
+    Endpoint.getUsers({
+      paginationPageNumber,
+      itemsCountPerPaginationPage: PER_PAGE_NUMBER,
+      searchByUserName: searchWard
+    }),
+    getUsersFetcher
+  )
+
+  const isLoading = !data && !error;
+
+  const heading = (): string => {
+    if (searchWard === "") {
+      return "ユーザー一覧";
+    }
+
+    return `"${searchWard}"の検索結果一覧`
+  }
+
+
+  const usersData = (): JSX.Element | null => {
+
+    if (isLoading) {
+      return (
+        <div className={styles.usersCardsFlow}>
+          <p>ローディング中</p>
+        </div>
+      )
+    }
+
+    if (error) {
+      return <p>エラーです</p>
+    }
+
+    if (data && data.totalItemsCount === 0) {
+      return <p>ユーザーはいません</p>
+    }
+
+    if (data && data.itemsCountInSelection === 0) {
+      return <p>検索結果なし</p>
+    }
+
+    if (data) {
+      return (
+        <>
+          <div className={styles.userCardsFlow}>
+            {data.users.map((user) => (
+              <UserCard targetUser={user}/>
+            ))}
+          </div>
+          <Pagination
+            className={styles.pagination}
+            totalCount={data.totalItemsCount}
+            currentPageNumber={paginationPageNumber}
+            perPageNumber={PER_PAGE_NUMBER}
+            setStatePageNumber={setPaginationPageNumber}
+          />
+        </>
+      )
+    }
+
+    return null;
+
+  }
+
+
   return (
-    <div>ユーザー一覧のページです</div>
+    <>
+      <div className={styles.usersListPage}>
+        <SearchInputField setStateSearchWard={setSearchWard}/>
+        <h1 className={styles.heading}>{ heading() }</h1>
+        {usersData()}
+      </div>
+    </>
   )
 }
+
 
 export default UsersListPage;

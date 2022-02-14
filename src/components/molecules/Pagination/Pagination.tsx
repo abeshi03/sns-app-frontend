1c6b969d21a470aa347a32ae15300e5f076030b7
abeshi03 <k_913411@yahoo.co.jp>
2022-02-14 15:17:34 +0900

    ページネーションコンポーネント作成完了

---
+++ src/components/molecules/Pagination/Pagination.tsx
@@ -0,0 +1,115 @@
+/* --- フレームワーク、ライブラリー --------------------------------------------------------------------------------------- */
+import React, { Dispatch, memo, SetStateAction, VFC } from "react";
+
+/* --- アセット ------------------------------------------------------------------------------------------------------- */
+import styles from "./Pagination.module.scss";
+
+
+type Props = {
+  totalCount: number;
+  currentPageNumber: number;
+  perPageNumber: number;
+  setStatePageNumber:  Dispatch<SetStateAction<number>>
+  className?: string;
+}
+
+/* eslint-disable-next-line react/display-name */
+export const Pagination: VFC<Props> = memo((props) => {
+
+  const { totalCount, currentPageNumber, perPageNumber, setStatePageNumber, className } = props;
+
+  const NUMBERED_BUTTONS_COUNT__MUST_BE_ODD: number = 2;
+  const totalPageCount = Math.ceil(totalCount / perPageNumber);
+
+  const mustDisplayGoToFirstPageButton = (): boolean => {
+    return currentPageNumber - (0.5 * (NUMBERED_BUTTONS_COUNT__MUST_BE_ODD - 1)) > 1;
+  };
+
+  const mustDisplayGoToLastPageButton = (): boolean => {
+    return currentPageNumber < totalPageCount - (0.5 * (NUMBERED_BUTTONS_COUNT__MUST_BE_ODD - 1));
+  };
+
+  const totalPagesCount = (): number => {
+    return Math.ceil(totalCount / perPageNumber);
+  }
+
+  return (
+    <>
+      { totalPageCount > 1 &&
+        <nav className={`${styles.pagination} ${className}`} >
+
+          { mustDisplayGoToFirstPageButton() &&
+            <span
+              className={styles.toFirstPageButton}
+              role="button"
+              aria-label="最初のページへ戻る"
+              onClick={() => setStatePageNumber(1)}
+            >{ "<< 最初" }</span>
+          }
+
+          { currentPageNumber > 1 &&
+            <span
+              className={styles.toPreviousPageButton}
+              role="button"
+              aria-label="一つ前のページへ戻る"
+              onClick={() => setStatePageNumber(prevState => prevState - 1)}
+            >{ "< 前" }</span>
+          }
+
+          { currentPageNumber > 2 &&
+            <span
+              role="button"
+              className={styles.PageNumberButton}
+              onClick={() => setStatePageNumber(currentPageNumber - 2)}
+            >{ currentPageNumber - 2 }</span>
+          }
+
+          { currentPageNumber > 1 &&
+            <span
+              role="button"
+              className={styles.PageNumberButton}
+              onClick={() => setStatePageNumber(currentPageNumber - 1)}
+            >{ currentPageNumber - 1 }</span>
+          }
+
+          <span className={styles.currentPageNumber}>{ currentPageNumber }</span>
+
+          { currentPageNumber + 1 <= totalPageCount &&
+            <span
+              role="button"
+              className={styles.PageNumberButton}
+              onClick={() => setStatePageNumber(currentPageNumber + 1)}
+            >{ currentPageNumber + 1 }</span>
+          }
+
+          { currentPageNumber + 2 <= totalPageCount &&
+            <span
+              role="button"
+              className={styles.PageNumberButton}
+              onClick={() => setStatePageNumber(currentPageNumber + 2)}
+            >{ currentPageNumber + 2 }</span>
+          }
+
+          { currentPageNumber !== totalPageCount &&
+            <span
+              role="button"
+              aria-label="次のページへ"
+              className={styles.toNextPageButton}
+              onClick={() => setStatePageNumber(prevState => prevState + 1)}
+            >{ "> 次" }</span>
+          }
+
+          { mustDisplayGoToLastPageButton() &&
+            <span
+              role="button"
+              aria-label="最後のページへ"
+              className={styles.toLastPageButton}
+              onClick={() => setStatePageNumber(totalPagesCount)}
+            >{ ">> 最後" }</span>
+          }
+
+        </nav>
+      }
+    </>
+  );
+});

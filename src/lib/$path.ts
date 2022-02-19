b9db6ea83d923bf6d753661aecb7df698e81d9c1
abeshi03 <k_913411@yahoo.co.jp>
2022-02-19 15:13:25 +0900

    inputFiledコンポーネントを追加

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -3,6 +3,9 @@
 export const pagesPath = {
   users: {
     _userId: (userId: string | number) => ({
+      edit: {
+        $url: (url?: { hash?: string }) => ({ pathname: '/users/[userId]/edit' as const, query: { userId }, hash: url?.hash })
+      },
       $url: (url?: { hash?: string }) => ({ pathname: '/users/[userId]' as const, query: { userId }, hash: url?.hash })
     }),
     $url: (url?: { hash?: string }) => ({ pathname: '/users' as const, hash: url?.hash })
---
+++ src/pages/users/[userId]/edit/index.page.tsx
@@ -0,0 +1,11 @@
+import { VFC } from "react";
+
+const userEditPage: VFC = () => {
+  return (
+    <div>
+      <h1>ユーザー編集</h1>
+    </div>
+  )
+}
+
+export default userEditPage;

1862dbd391b1c70e8132ceb0fd4c25250cc2d5ed
abeshi03 <k_913411@yahoo.co.jp>
2022-03-06 22:59:39 +0900

    検索の処理を追加

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,5 @@
 /* --- ライブラリー --------------------------------------------------------------------------------------------------- */
-import React, { useEffect, useRef } from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import useSWRInfinite from "swr/infinite";
 
 /* --- アセット ------------------------------------------------------------------------------------------------------ */
@@ -16,6 +16,7 @@ import { getPostsFetcher } from "../apis/PostsApi";
 
 /* --- 子コンポーネント ------------------------------------------------------------------------------------------------ */
 import { PostCard } from "../components/organisms/Card/PostCard/PostCard";
+import { SearchInputField } from "../components/atoms/SearchInputField/SearchInputField";
 
 /* --- 補助関数 ------------------------------------------------------------------------------------------------------- */
 import { useIntersection } from "../utility/intersection";
@@ -24,27 +25,40 @@ import { useIntersection } from "../utility/intersection";
 
 export default function App() {
 
+  /* --- state ------------------------------------------------------------------------------------------------------- */
+  const [ searchWard, setSearchWard ] = useState("");
+
+  /* --- 検索関連 ----------------------------------------------------------------------------------------------------- */
+  const searchByPostContent = useCallback((searchWard: string): void => {
+    setSearchWard(searchWard);
+  }, [])
+
+  /* --- 無限スクロール処理 --------------------------------------------------------------------------------------------- */
   const ref = useRef<HTMLDivElement>(null) as React.MutableRefObject<HTMLDivElement>;
 
   const intersection = useIntersection(ref);
 
-  const limit = 2;
+  const limit = 15;
 
   // useSWRInfiniteのキーとなるパラメータ付きURLを生成
   const getKey = (pageNumber: number, previousPageData: Post[]) => {
     if (previousPageData && !previousPageData.length) return null;
     // pageIndexは0からのため+1をしてpageIndexを1からにする
-    return Endpoint.getPosts({ pageNumber: pageNumber + 1, limit })
+    return Endpoint.getPosts({
+      pageNumber: pageNumber + 1,
+      limit,
+      searchByPostContent: searchWard
+    });
   };
 
 
+  /* useSWRInfiniteで無限スクロール用に投稿取得 */
   const { data: posts, error, size, setSize } = useSWRInfinite( getKey, getPostsFetcher, { initialSize: 2 });
 
   const isEmpty = posts?.[0]?.length === 0;
   const isReachingEnd = isEmpty || (posts && posts[posts.length - 1]?.length < limit);
 
 
-  // 次のデータの取得
   const getNextDataPosts = async () => {
     await setSize(size + 1);
   };
@@ -63,8 +77,14 @@ export default function App() {
 
   const postList = posts.flat();
 
+  console.log("際レンダリング")
+
   return (
     <div className={styles.homePage}>
+
+      <SearchInputField
+        onSearchFunction={searchByPostContent}
+      />
 
       <div className={styles.postCardsFlow}>
         {postList.map((post, index) => (

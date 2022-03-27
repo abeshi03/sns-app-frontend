8bddca985fcc6b5dbc287648354727439bb03130
abeshi03 <k_913411@yahoo.co.jp>
2022-03-27 10:45:23 +0900

    サイドバー修正

--- src/components/layouts/SideBar/SideBar.tsx
+++ src/components/layouts/SideBar/SideBar.tsx
@@ -1,7 +1,11 @@
 /* --- ライブラリー、フレームワーク --------------------------------------------------------------------------------------- */
-import { VFC, memo, ReactNode, useState } from "react";
+import React, { VFC, memo, ReactNode, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
+
+/* --- グローバルstate ------------------------------------------------------------------------------------------------ */
+import { currentUserState } from "../../../store/auth/currentUserState";
 
 /* --- アセット ------------------------------------------------------------------------------------------------------- */
 import styles from "./SideBar.module.scss";
@@ -13,6 +17,9 @@ import { pagesPath } from "../../../lib/$path";
 /* --- 型定義 -------------------------------------------------------------------------------------------------------- */
 import { UrlObject } from "url";
 
+/* --- 補助関数 ------------------------------------------------------------------------------------------------------- */
+import { isNotNull } from "../../../utility/typeGuard/isNotNull";
+import { isNull } from "../../../utility/typeGuard/isNull";
 
 
 type Props = {
@@ -23,31 +30,9 @@ type Navigation = {
   pageName: string;
   path: UrlObject;
   icon: JSX.Element;
+  isDisplay: boolean;
+  onClickFunction?: () => void;
 }
-
-const navigations: Navigation[] = [
-  {
-    pageName: "トップ",
-    path: pagesPath.$url(),
-    icon: <HomeIcon className={styles.icon}/>
-  },
-  {
-    pageName: "ユーザー一覧",
-    path: pagesPath.users.$url(),
-    icon: <HomeIcon className={styles.icon}/>
-  },
-  {
-    pageName: "ユーザー追加",
-    path: pagesPath.users.add.$url(),
-    icon: <HomeIcon className={styles.icon}/>
-  },
-  {
-    pageName: "ログイン",
-    path: pagesPath.sign_in.$url(),
-    icon: <HomeIcon className={styles.icon}/>
-  },
-];
-
 
 
 export const SideBar: VFC<Props> = memo((props) => {
@@ -55,8 +40,43 @@ export const SideBar: VFC<Props> = memo((props) => {
   const { children } = props;
 
   const [ menuOpen, setMenuOpen ] = useState(true);
-
+  const currentUser = useRecoilValue(currentUserState).currentUser;
   const router = useRouter();
+
+  const navigations: Navigation[] = [
+    {
+      pageName: "トップ",
+      path: pagesPath.$url(),
+      icon: <HomeIcon className={styles.icon}/>,
+      isDisplay: true
+    },
+    {
+      pageName: "ユーザー一覧",
+      path: pagesPath.users.$url(),
+      icon: <HomeIcon className={styles.icon}/>,
+      isDisplay: true
+    },
+    {
+      pageName: "ユーザー追加",
+      path: pagesPath.users.add.$url(),
+      icon: <HomeIcon className={styles.icon}/>,
+      isDisplay: true
+    },
+    {
+      pageName: "ログイン",
+      path: pagesPath.sign_in.$url(),
+      icon: <HomeIcon className={styles.icon}/>,
+      isDisplay: isNull(currentUser)
+    },
+    {
+      pageName: "ログアウト",
+      path: {},
+      icon: <HomeIcon className={styles.icon}/>,
+      isDisplay: isNotNull(currentUser),
+      onClickFunction: () => {}
+    },
+  ];
+
 
   const isPageActive = (pagePath: UrlObject): boolean => {
     return pagePath.pathname === router.route
@@ -73,14 +93,29 @@ export const SideBar: VFC<Props> = memo((props) => {
           ))}
         </div>
         {navigations.map((navigation) => (
-          <Link href={navigation.path} key={navigation.pageName}>
-            <a
-              className={styles.flexContainer}
-              style={{ background: isPageActive(navigation.path) ? "#027fce" : "none" }}>
-              { navigation.icon }
-              { menuOpen && <p className={styles.pageName}>{ navigation.pageName }</p> }
-            </a>
-          </Link>
+          <React.Fragment key={navigation.pageName}>
+            {navigation.isDisplay && !navigation.onClickFunction &&
+              <Link href={navigation.path}>
+                <a
+                  className={styles.flexContainer}
+                  role="navigation"
+                  style={{ background: isPageActive(navigation.path) ? "#027fce" : "none" }}>
+                  { navigation.icon }
+                  { menuOpen && <p className={styles.pageName}>{ navigation.pageName }</p> }
+                </a>
+              </Link>
+            }
+            {navigation.isDisplay && navigation.onClickFunction &&
+              <p
+                className={styles.flexContainer}
+                role="navigation"
+                onClick={navigation.onClickFunction}
+              >
+                { navigation.icon }
+                { menuOpen && <p className={styles.pageName}>{ navigation.pageName }</p> }
+              </p>
+            }
+          </React.Fragment>
         ))}
       </aside>
 

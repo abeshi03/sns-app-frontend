6b86b42fca05c5e89470d3b751f13d941229c362
abeshi03 <k_913411@yahoo.co.jp>
2022-04-19 22:48:24 +0900

    fix

---
+++ src/pages/users/[userId]/edit/_middleware.page.ts
@@ -0,0 +1,8 @@
+// import {NextResponse} from "next/server";
+// import {isNull} from "../../../../utility/typeGuard/isNull";
+//
+// export function middleware() {
+//   if (isNull(externalReferenceUser)) {
+//     return NextResponse.redirect("/sign_in");
+//   }
+// }

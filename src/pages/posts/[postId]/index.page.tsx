97cf116e36ca145e95926883924a1485643530d7
abeshi03 <k_913411@yahoo.co.jp>
2022-04-22 00:57:41 +0900

    fix

--- src/pages/posts/[postId]/index.page.tsx
+++ src/pages/posts/[postId]/index.page.tsx
@@ -11,24 +11,25 @@ import { Endpoint } from "../../../constants/endpoints";
 import { getPostFetcher } from "../../../apis/PostsApi";
 
 /* --- 型定義 --------------------------------------------------------------------------------------------------------- */
-import {Post} from "../../../type/Post";
+import { Post } from "../../../type/Post";
+
 
 const PostDetailsPage: NextPage = () => {
 
   const router = useRouter();
-  /* １回目のレンダリングのrouter.queryはundefinedの為、isReadyでクライアントで使える状態を確認する */
-  if (!router.isReady) {
-    return null
-  }
   const { postId } = router.query;
 
-
   /* --- 投稿取得 ----------------------------------------------------------------------------------------------------- */
-  const { data, error } = useSWR<Post>(Endpoint.getPost({ postId: Number(postId) }));
+  /* router.queryは初回レンダリングがundefinedのため考慮 */
+  const { data: post, error } = useSWR<Post>(postId ? Endpoint.getPost({ postId: Number(postId) }) : null, getPostFetcher);
+  const isPostLoading = !post && !error;
+  const isPostError = error;
 
   return (
     <div className={styles.postDetailsPage}>
-      <p>詳細</p>
+      {isPostLoading && <p>ローディング</p>}
+      {isPostError && <p>エラーです</p>}
+      {post && <p>{ post.content }</p>}
     </div>
   );
 }

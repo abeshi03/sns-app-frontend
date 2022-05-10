d7f3978468a568a808085d95b493f303231ff209
abeshi03 <k_913411@yahoo.co.jp>
2022-05-11 00:12:37 +0900

    tab: 基本マークアップ完了

---
+++ src/components/atoms/Tab/Tab.tsx
@@ -0,0 +1,52 @@
+/* --- ライブラリー ---------------------------------------------------------------------------------------------------- */
+import React, { memo, VFC } from "react";
+
+/* --- アセット ------------------------------------------------------------------------------------------------------- */
+import styles from "./Tab.module.scss"
+
+namespace Tabs {
+  type TabCommonProperties = {
+    readonly label: string;
+    readonly selected?: boolean;
+  };
+
+  export type TabIdentifiedByKey =
+    TabCommonProperties &
+    {
+      readonly key: string | number;
+    };
+
+  export type TabIdentifiedByEntity =
+    TabCommonProperties &
+    {
+      readonly relatedEntity: unknown;
+    };
+  export type Tab = TabIdentifiedByKey | TabIdentifiedByEntity;
+}
+
+type Props = {
+  tabs: Tabs.Tab[];
+  disabled?: boolean;
+  tabClickFunction?: () => void;
+}
+
+export const Tab: VFC<Props> = memo((props) => {
+
+  const { tabs, disabled, tabClickFunction } = props;
+
+  return (
+    <ul className={styles.tabs} role="tablist">
+      {tabs.map((tab: Tabs.Tab, index: number) => (
+        <li
+          className={styles.tab}
+          role="tab"
+          key={`TAD-${index}`}
+          tabIndex={0}
+          onClick={tabClickFunction}
+        >
+          { tab.label }
+        </li>
+      ))}
+    </ul>
+  );
+});
